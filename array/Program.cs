11dd999b83b451675d78d1b369539159301ca066
gustavekd <gustav.ekelund99@gmail.com>
2016-10-27 18:02:20 +0200

    arrays övn

--- array/Program.cs
+++ array/Program.cs
@@ -10,6 +10,45 @@ namespace array
     {
         static void Main(string[] args)
         {
+            //övning1
+            /* string[] namn = new string[10];
+
+            for (int i = 0; i < 10 ; i++)
+            {
+                   Console.Write("Mata in ett namn:");
+                    namn[i] = Console.ReadLine();
+                }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(namn[i]);
+            } */
+            //övning2 
+            /*
+            int[] tal = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("mata in ett tal:" + " ");
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(tal[i]);
+            } */
+
+            //övning3
+
+            int[] numbArray = { 12, 5, 6, 1, 2, 3, -2, -7, 0 };
+
+            for (int a = 0; a < numbArray.Length; a++)
+            {
+                for (int b = a; b < numbArray.Length; b++)
+                {
+                    if (numbArray[b] < numbArray[a])
+                    {
+                        Console.WriteLine(numbArray[a]);
+                    }
+                    }
+            }
         }
     }
 }
